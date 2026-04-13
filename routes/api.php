dbf174666fed04ab5cfe544ac24548b59f8f0e34
John Araya Echeverria <johnmai1205@gmail.com>
2026-04-13 15:51:44 -0400

    Se agrego el modulo completo de Login y Logout,tanto como AuthController como LoginRequest, ademas se actualizo las rutas y el modulo de User para agregarle poder realizar login y logout

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 // Restaurar usuario eliminado
 Route::patch('users/{id}/restore', [UserController::class, 'restore']);
