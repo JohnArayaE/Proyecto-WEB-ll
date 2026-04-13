f6c04277bb9bcf2c5d9338f9f555acb238b77ee3
Miguel Cruz <miguelcruzvargas12@gmail.com>
2026-04-12 22:29:12 -0600

    implementacion de policies y a vehiculos

--- routes/api.php
+++ routes/api.php
@@ -8,12 +8,20 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::patch('vehicle/{id}/restore', [VehicleController::class, 'restore']);
+Route::patch('vehicle/{vehicle}/restore', [VehicleController::class, 'restore'])
+    ->withTrashed()
+    ->middleware('auth:sanctum', 'can:restore,vehicle');
 
 Route::apiResource('vehicle', VehicleController::class)
+    ->middleware('auth:sanctum')
+    ->middlewareFor('index', 'can:viewAny,App\Models\Vehicle')
+    ->middlewareFor('show', 'can:view,vehicle')
+    ->middlewareFor('store', 'can:create,App\Models\Vehicle')
+    ->middlewareFor('update', 'can:update,vehicle')
+    ->middlewareFor('destroy', 'can:delete,vehicle')
     ->missing(function (Request $request) {
         return response()->json([
-            'message' => 'vehiculo no encontrado.',
+            'message' => 'Usuario no encontrado.',
         ], 404);
     });
 
